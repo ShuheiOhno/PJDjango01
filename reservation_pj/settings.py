d6c92d8fe0a28731cbc975cfc1d9b532928de806
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-08-27 16:07:39 +0900

    Update settings.py

--- reservation_pj/settings.py
+++ reservation_pj/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'app',
+    'django.contrib',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
