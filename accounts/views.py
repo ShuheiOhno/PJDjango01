3f22d53903ca7d757c1b21fe6e0457e0db935120
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-08-27 20:42:31 +0900

    profile view

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from accounts.models import CustomUser
 
-# Create your views here.
 class ProfileView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'accounts/profile.html')
-    
-    
+        #ログイン中ユーザーの取得
+        user_data = CustomUser.objects.get(id=request.user.id)
+
+        return render(request, 'accounts/profile.html', {
+            'user_data': user_data,
+        })
