2bc45d501083d362798b99849307536b84bf53b8
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-08-27 22:24:37 +0900

    signup,redirect

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,19 @@
 from django import forms
+from allauth.account.forms import SignupForm
+
+class SignupUserForm(SignupForm):
+    #サインアップ時に必要なフォーム
+    first_name = forms.CharField(max_length=50, label='姓')
+    last_name = forms.CharField(max_length=50, label='名')
+    print('aaa')
+
+    def save(self, request):
+        user = super(SignupUserForm, self).save(request)
+        print(user)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
+        return user
 
 class ProfileForm(forms.Form):
     first_name = forms.CharField(max_length=50, label='姓')
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'app/index.html'
+    login_url = '/account/login/'
 
 
