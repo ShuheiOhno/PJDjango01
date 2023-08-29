b425d2786dc229df33e6235f4c8d2305c7c26803
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-08-29 21:37:26 +0900

    store
    ホームは店一覧

--- app/views.py
+++ app/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from app.models import Store
 
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'app/index.html'
     login_url = '/account/login/'
 
+class StoreView(View):
+    def get(self, request, *args, **kwargs):
+        store_data = Store.objects.all()
+
+        return render(request, 'app/store.html', {
+            'store_data': store_data,
+        })
 
