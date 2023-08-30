f5f1d6ce2ddebc955dea32a7f40b6bd9a604ccce
ShuheiOhno <57937334+ShuheiOhno@users.noreply.github.com>
2023-08-30 19:20:29 +0900

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Store, Staff
+from .models import Store, Staff, Booking
 
 # Register your models here.
 admin.site.register(Store)
 admin.site.register(Staff)
+admin.site.register(Booking)
